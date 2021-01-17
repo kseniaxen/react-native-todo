ab8c2c496c54b0412837d2cbc717c3652a2c7a0f
Ksenia Serdiuk <serdiuk.xen@gmail.com>
2021-01-17 22:30:43 +0200

    Add some data

--- src/Components/TodoList/index.js
+++ src/Components/TodoList/index.js
@@ -1,12 +1,49 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, FlatList} from 'react-native'
+
+const todoData = [
+    {
+        id:'1',
+        name:'Hand sanitizer',
+        category:'Shopping List'
+    },
+    {
+        id:'2',
+        name:'Upload 1099-R to TurboTax',
+        category:'Finance'
+    },
+    {
+        id:'3',
+        name:'Submit 2019 tax return',
+        category:'Finance'
+    },
+    {
+        id:'4',
+        name:'Print parking passes',
+        category:'Wedding'
+    },
+    {
+        id:'5',
+        name:'Sign contract, send back',
+        category:'Freelance'
+    }
+]
 
 export const TodoList = ()=>{
+
+    const renderItem = ({item}) =>(
+        <View>
+            <Text>{item.name}</Text>
+            <Text>{item.category}</Text>
+        </View>
+    )
+
     return (
         <View>
-            <Text>
-                Список
-            </Text>
+            <FlatList 
+            data={todoData}
+            renderItem={renderItem}
+            keyExtractor={item=>item.id}/>
         </View>
     )
 }
