fdd4ff01aa4384e5e3b2b7c8d559ac80123fae0a
Ksenia Serdiuk <serdiuk.xen@gmail.com>
2021-01-17 22:17:58 +0200

    Init elements

---
+++ src/Components/TodoList/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+export const TodoList = ()=>{
+    return (
+        <View>
+            <Text>
+                Список
+            </Text>
+        </View>
+    )
+}
---
+++ src/Screens/Main/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View } from 'react-native'
+import { TodoList } from '../../Components/TodoList'
+
+export const Main = () => {
+    return (
+        <View>
+            <TodoList/>
+        </View>
+    );
+}
