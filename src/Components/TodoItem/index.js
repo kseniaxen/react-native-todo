28ec4a3dfc3143bffa4d84c91683a1f6aa5331dc
Ksenia Serdiuk <serdiuk.xen@gmail.com>
2021-01-25 13:47:19 +0200

    Edit name task

--- src/Components/TodoItem/index.js
+++ src/Components/TodoItem/index.js
@@ -1,25 +1,43 @@
 import React, { useState } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 import CheckBox from '@react-native-community/checkbox'
-import {todoProps} from '../../Constants/todo'
+import { todoProps } from '../../Constants/todo'
 
 export const TodoItem = ({ id, name, category, onChangeTodo, isCompleted }) => {
 
     const [toggleCheckBox, setToggleCheckBox] = useState(isCompleted)
+    const [isEditable, setIsEditable] = useState(false)
 
-    const onChange = (key, value) =>{
-        setToggleCheckBox(value)
-        onChangeTodo({key, value, id})
+    const onChange = (key, value) => {
+        if (key == todoProps.isCompleted) {
+            setToggleCheckBox(value)
+        }
+        onChangeTodo({ key, value, id })
     }
     return (
         <View style={styles.container}>
-            <CheckBox
-                disabled={false}
-                value={toggleCheckBox}
-                onValueChange={(value) => onChange(todoProps.isCompleted,value)} />
-            <View style={styles.textContainer}>
-                <Text style={styles.nameText}>{name}</Text>
-                <Text style={styles.categoryText}>{category}</Text>
+            <View style={styles.infoContainer}>
+                <CheckBox
+                    disabled={false}
+                    value={toggleCheckBox}
+                    onValueChange={(value) => onChange(todoProps.isCompleted, value)} />
+                <View style={styles.textContainer}>
+                    {isEditable ? (
+                        <TextInput
+                            onChangeText={(value) => onChange(todoProps.name, value)}
+                            defaultValue={name}
+                            style={styles.nameText}
+                        />
+                    ) : (
+                            <Text style={styles.nameText}>{name}</Text>
+                        )}
+                    <Text style={styles.categoryText}>{category}</Text>
+                </View>
+            </View>
+            <View>
+                <TouchableOpacity onPress={() => setIsEditable(!isEditable)}>
+                    <Text>Ред.</Text>
+                </TouchableOpacity>
             </View>
         </View>
     )
@@ -28,9 +46,13 @@ export const TodoItem = ({ id, name, category, onChangeTodo, isCompleted }) => {
 const styles = StyleSheet.create({
     container: {
         paddingVertical: 10,
+        justifyContent:'space-between',
+        flexDirection: 'row'
+    },
+    infoContainer:{
         flexDirection:'row'
     },
-    textContainer:{
+    textContainer: {
         marginLeft: 15
     },
     nameText: {
